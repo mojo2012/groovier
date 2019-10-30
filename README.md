9801e49dcaa16c02007c642ccb8a53028ab3b3a4
Matthias Fuchs <matthias.fuchs@kps.com>
2019-10-30 19:38:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Groovier
+Groovier adds some useful groovy extensions to often-used classes:
+* `String`: mostly stuff from `apache-commons-lang`
---
+++ src/main/groovy/io/spotnext/groovier/StringExtension.groovy
@@ -0,0 +1,32 @@
+package io.spotnext.groovier
+
+import groovy.transform.CompileStatic
+import org.apache.commons.lang3.StringUtils
+
+@CompileStatic
+class StringExtension {
+
+    static String trimToNull(String string) {
+        return StringUtils.trimToNull(string)
+    }
+
+    static boolean isBlank(String string) {
+        return StringUtils.isBlank(string)
+    }
+
+    static boolean isNotBlank(String string) {
+        return !isBlank(string)
+    }
+
+    static boolean isEmpty(String string) {
+        return StringUtils.isEmpty(string)
+    }
+
+    static boolean iNotEmpty(String string) {
+        return !isEmpty(string)
+    }
+
+    static boolean isNull(String string) {
+        return string == null
+    }
+}
